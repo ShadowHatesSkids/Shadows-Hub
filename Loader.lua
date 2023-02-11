2afe2a97a55ce7f829ad6f0e2292142feb5221d3
Shadow <91681532+ShadowHatesSkids@users.noreply.github.com>
2023-02-11 17:28:23 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,9 @@
-
-
+local GamesList = {}
 local Universal = "https://raw.githubusercontent.com/ShadowHatesSkids/Shadows-Hub/main/Universal.lua"
 local PID = game.PlaceId
 
 if not GamesList[PID] then
-    return loadstring(Universal)
+    print("Abc")
 else
     return loadstring(GamesList[PID])
 end
