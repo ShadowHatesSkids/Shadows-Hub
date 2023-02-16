52df5e91961436f726c63ff8634a8c11cfd36169
ShadowHatesSkids <p1xel9413@gmail.com>
2023-02-16 17:46:54 +0000

    a

--- Loader.lua
+++ Loader.lua
@@ -3,8 +3,8 @@ local SupportedGames = {}
 local PID = game.PlaceId
 local Universal = "https://raw.githubusercontent.com/ShadowHatesSkids/Shadows-Hub/main/Universal.lua"
 
-if SupportedGames[PID] then
-    loadstring(game:HttpGet(SupportedGames[PID]))
-else
+--if SupportedGames[PID] then
+   -- loadstring(game:HttpGet(SupportedGames[PID]))
+--else
     loadstring(game:HttpGet(Universal))
-end
+--end
