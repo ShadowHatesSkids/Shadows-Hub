a153fc230ff78f5759828e43d3330397f6e930c9
ShadowHatesSkids <p1xel9413@gmail.com>
2023-02-16 17:39:16 +0000

    Merge branch 'main' of https://github.com/ShadowHatesSkids/Shadows-Hub

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,10 @@
-print("abc")
+local SupportedGames = {}
+
+local PID = game.PlaceId
+local Universal = "https://raw.githubusercontent.com/ShadowHatesSkids/Shadows-Hub/main/Universal.lua"
+
+if SupportedGames[PID] then
+    loadstring(game:HttpGet(SupportedGames[PID]))
+else
+    loadstring(game:HttpGet(Universal))
+end
